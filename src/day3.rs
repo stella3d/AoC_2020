5b38cc4bf66b028c0d80a66be06c6a5e743eb9f7
Stella Cannefax <stella@unity3d.com>
2020-12-03 01:50:05 -0800

    setup day 3 w/ stub

---
+++ src/day3.rs
@@ -0,0 +1,14 @@
+use crate::files;
+
+fn solution(lines: Vec<String>) {
+    let mut valid_count = 0;
+    for line in lines {
+
+    }
+
+    println!("day 3 - VALID COUNT: {}", valid_count);
+}
+
+pub(crate) fn run() {
+    solution(files::get_lines("./input/3in.txt").unwrap());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod files;
 mod day2;
+mod day3;
 
 fn main() {
-    //day2::run();
+    day3::run();
 }
